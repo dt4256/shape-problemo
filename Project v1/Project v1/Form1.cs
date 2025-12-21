3df656820bdb105c4ad61c8bc99dbf5cce421617
Dt4_256 <ihromushin.y177@gmail.com>
2025-12-21 18:17:06 +0300

    added outer shell things

--- Project v1/Project v1/Form1.cs
+++ Project v1/Project v1/Form1.cs
@@ -16,13 +16,24 @@ using System.Windows.Forms;
 
 namespace Project_v1
 {
+    public enum Figures
+    {
+        Circle,
+        Square,
+        Triangle
+    }
+    public enum Algos
+    {
+        Basic,
+        Mod
+    }
     public partial class Form1 : Form
     {
-        string nowFigure = "Circle";//circle,sqare,triangle
+        Figures nowFigure = Figures.Circle;
+        Algos Algo = Algos.Basic;
         List<(int,int)> dots_lines = new List<(int,int)>();
-        
         List<Shape> shapes = new List<Shape>();
-       
+        
         public Form1()
         {
             InitializeComponent();
@@ -30,8 +41,53 @@ namespace Project_v1
             shapes.Add(new Circle(300,300));
         }
 
-        private double Get_K(int x1, int y1, int x2, int y2) {
+
+        private void RecalculateHullBasic()
+        {
+            dots_lines.Clear();   
+            for (int i = 0; i < shapes.Count; i++)
+                shapes[i].Status = 0;
+
+            dots_lines.Clear();
+
+            for (int i = 0; i < shapes.Count; i++)
+            {
+                for (int j = 0; j < shapes.Count; j++)
+                {
+                    if (i == j) continue;
+
+                    bool upper = false;
+                    bool lower = false;
+
+                    double[] tmp = Get_K(shapes[i].X, shapes[i].Y,
+                                         shapes[j].X, shapes[j].Y);
+
+                    double k = tmp[0];
+                    int x1 = (int)tmp[1];
+                    int y1 = (int)tmp[2];
+
+                    for (int m = 0; m < shapes.Count; m++)
+                    {
+                        int pos = Upper_Lower(shapes[m].X, shapes[m].Y, x1, y1, k);
+                        if (pos == 1) upper = true;
+                        else if (pos == -1) lower = true;
+                    }
+
+                    if ((upper && !lower) || (!upper && lower) || (!upper && !lower))
+                    {
+                        shapes[i].Status = 1;
+                        shapes[j].Status = 1;
+                        dots_lines.Add((i, j));
+                    }
+                }
+            }
+        }
+
+
+
+        private double[] Get_K(int x1, int y1, int x2, int y2) {
             double k;
+            int tx1 = x1,ty1 = y1;
             if (x1 > x2)
             {
                 (x1, x2) = (x2, x1);
@@ -41,12 +97,14 @@ namespace Project_v1
             y2 -= y1;
             x1 = 0;y1 = 0;
             //y2=k*x2 k =y2/x2
-            k = (double)(y2) / x2;
-
-            return k;
+            if (x2 == 0) { k = double.PositiveInfinity; }
+            else { k = (double)(y2) / x2; }
+            return new double[] { k, tx1, ty1 };
         }
-        private int Upper_Lower(int x, int y, double k)
+        private int Upper_Lower(int x, int y, int x1,int y1, double k)
         {
+            x -= x1;
+            y -= y1;
             if (y > k * x)
             {
                 return 1;
@@ -81,53 +139,34 @@ namespace Project_v1
             }
                 if (e.Button == MouseButtons.Left && temp == false)
                 {
-                    if (nowFigure == "Circle")
+                    if (nowFigure == Figures.Circle)
                     {
                         shapes.Add(new Circle(e.X, e.Y));
                         shapes[shapes.Count - 1].Flag = true;
                         
                         Refresh();
                     }
-                    else if (nowFigure == "Sqare")
+                    else if (nowFigure == Figures.Square)
                     {
                         shapes.Add(new Sqare(e.X, e.Y));
                         shapes[shapes.Count - 1].Flag = true;
                         
                         Refresh();
                     }
-                    else if (nowFigure == "Triangle")
+                    else if (nowFigure == Figures.Triangle)
                     {
                         shapes.Add(new Triangle(e.X, e.Y));
                         shapes[shapes.Count - 1].Flag = true;
                         Refresh();
                     }
-                for (int i = 0; i < shapes.Count; i++) {
-                    shapes[i].Status = 0;
+                //перерисовка оболочки
+                if (shapes.Count > 2 && Algo == Algos.Basic)
+                {
+                    RecalculateHullBasic();
                 }
-                for (int i = 0; i < shapes.Count; i++) {
-                    for (int j = 0; j < shapes.Count; j++) {
-                        //2 вершины с которыми сравнивать
-                        if (i == j) continue;
-                        bool upper = false;
-                        bool lower = false;
-                        for (int m = 0; m < shapes.Count; m++) {
-                            double k;
-                            k = Get_K(shapes[i].X, shapes[i].Y, shapes[j].X, shapes[j].Y);
-                            int tmp = Upper_Lower(shapes[m].X, shapes[m].Y, k);
-                            if (tmp == 1) upper = true;
-                            else if(tmp== -1) lower = true;
-                            
-                        }
-                        if((upper && !lower) || (!upper && lower) || (!upper && !lower))
-                        {
-                            shapes[i].Status = 1;
-                            shapes[j].Status = 1;
-                            dots_lines.Add((i, j));
-                        }
-                    }
-                }
-                
-                           
+                //перерисовка оболочки
+                Refresh();
+
             }
 
             for (int i = shapes.Count-1; i >=0; i--)
@@ -135,11 +174,18 @@ namespace Project_v1
                 if (e.Button == MouseButtons.Right && shapes[i].IsInside(e.X, e.Y))
                 {
                     shapes.RemoveAt(i);
-                    
+                    //перерисовка оболочки
+                    if (shapes.Count > 2 && Algo == Algos.Basic)
+                    {
+                        RecalculateHullBasic();
+                    }else dots_lines.Clear();
                     Refresh();
+                    //перерисовка оболочки
                     break;
                 }
             }
+
+            
         }
             
         
@@ -155,6 +201,13 @@ namespace Project_v1
                     Refresh();
                 }
             }
+            //не помню, нужно ли было по тз. Спросить в пн
+            if (shapes.Count > 2 && Algo == Algos.Basic)
+            {
+                RecalculateHullBasic();
+            }
+            Refresh();
+            //конец спорного
         }
         //клик
         private void Form1_MouseUp(object sender, MouseEventArgs e)
@@ -165,11 +218,18 @@ namespace Project_v1
                 shapes[i].DiffX = 0;
                 shapes[i].DiffY = 0;
             }
+            //перерисовка оболочки
+            if (shapes.Count > 2 && Algo == Algos.Basic)
+            {
+                RecalculateHullBasic();
+            }
+            //перерисовка оболочки
+            Refresh();
         }
 
         private void sqareToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            nowFigure = "Sqare";
+            nowFigure = Figures.Square;
             sqareToolStripMenuItem.Checked = true;
             circleToolStripMenuItem.Checked = false;
             triangleToolStripMenuItem.Checked = false;
@@ -178,7 +238,7 @@ namespace Project_v1
 
         private void circleToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            nowFigure = "Circle";
+            nowFigure = Figures.Circle;
             sqareToolStripMenuItem.Checked = false;
             circleToolStripMenuItem.Checked = true;
             triangleToolStripMenuItem.Checked = false;
@@ -186,7 +246,7 @@ namespace Project_v1
 
         private void triangleToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            nowFigure = "Triangle";
+            nowFigure = Figures.Triangle;
             sqareToolStripMenuItem.Checked = false;
             circleToolStripMenuItem.Checked = false;
             triangleToolStripMenuItem.Checked = true;
@@ -209,7 +269,7 @@ namespace Project_v1
             foreach(var(l,m ) in dots_lines)  {
                 Pen pen = new Pen(shapes[0].Clr);
                 e.Graphics.DrawLine(pen, shapes[l].X, shapes[l].Y, shapes[m].X,shapes[m].Y);
-            }dots_lines.Clear();
+            }
 
 
         }
